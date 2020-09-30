1448269f9ab45602e946fb7f757d9b87656a3c17
lelandshir <lelandshir@gmail.com>
2020-09-30 12:08:10 -0700

    +Menu, MenuItem, mult onClick how to, drop down menu, opacity, hover, styling

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -9,6 +9,9 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Button from "@material-ui/core/Button";
 import { Typography } from "@material-ui/core";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+
 //imports from mui-styles
 import { makeStyles } from "@material-ui/styles";
 
@@ -26,7 +29,9 @@ function ElevationScroll(props) {
 }
 
 const useStyles = makeStyles((theme) => ({
+	//see how simple it is to manage the styles with Material-UI
 	toolbarMargin: {
+		//taking note of the spread operator use here
 		...theme.mixins.toolbar,
 		marginBottom: "3em",
 	},
@@ -48,6 +53,18 @@ const useStyles = makeStyles((theme) => ({
 		marginRight: "25px",
 		height: "45px",
 	},
+	menu: {
+		backgroundColor: theme.palette.common.blue,
+		color: "white",
+		borderRadius: "0px",
+	},
+	menuItem: {
+		...theme.typography.tab,
+		opacity: 0.7,
+		"&:hover": {
+			opacity: 1,
+		},
+	},
 	logoContainer: {
 		padding: 0,
 		//stop the opacity overlay with the built in hoiver effect like so
@@ -60,12 +77,28 @@ const useStyles = makeStyles((theme) => ({
 export default function Header(props) {
 	const classes = useStyles();
 	const [value, setValue] = useState(0);
+	// create anchorEl and setAnchorEl using useState with default of "null"
+	const [anchorEl, setAnchorEl] = useState(null); //this is the state that stores the componenet we clicked on && where we want the menu to be rendered
+	const [open, setOpen] = useState(false); //determines visibility of the menu
 
 	const handleChange = (e, value) => {
 		setValue(value);
 	};
 
+	const handleClick = (e) => {
+		//event contains all the information about where we just clicked on screen
+		setAnchorEl(e.currentTarget); //will contain the elemenet we clciked on && tells menu where we want it to be rendered
+		setOpen(true); //tell menu to be rendered
+	};
+
+	const handleClose = (e) => {
+		//tell the menu to close
+		setAnchorEl(null);
+		setOpen(false);
+	};
+
 	useEffect(() => {
+		//to manage the browser running back to home after refresh
 		if (window.location.pathname === "/" && value !== 0) {
 			setValue(0);
 		} else if (window.location.pathname === "/services" && value !== 1) {
@@ -110,8 +143,12 @@ export default function Header(props) {
 								label="Home"
 							/>
 							<Tab
+								aria-owns={anchorEl ? "simple-menu" : undefined}
+								aria-haspopup={anchorEl ? "true" : undefined}
 								className={classes.tab}
 								component={Link}
+								onMouseOver={(event) => handleClick(event)}
+								//menu hangs around on mouseleave unless we target the menu list properties below
 								to="/services"
 								label="Services"
 							/>
@@ -141,6 +178,63 @@ export default function Header(props) {
 						>
 							Free Estimate
 						</Button>
+						<Menu
+							classes={{ paper: classes.menu }}
+							id="simple-menu"
+							anchorEl={anchorEl}
+							open={open}
+							onClose={handleClose}
+							//target the menu list via props to handle the menu closing on MouseLeave #material-ui
+							MenuListProps={{ onMouseLeave: handleClose }}
+							elevation={0}
+						>
+							<MenuItem
+								//this is how we can set multiple onClick functions; add another set of brackets, turn it into an arrow function, so that it becomes a full expression to call handleclose function, use a semilcolon and call setValue as well
+								onClick={() => {
+									handleClose();
+									setValue(1);
+								}}
+								component={Link}
+								to="/services"
+								classes={{ root: classes.menuItem }}
+							>
+								Services
+							</MenuItem>
+							<MenuItem
+								//this is how we can set multiple onClick functions; add another set of brackets, turn it into an arrow function, so that it becomes a full expression to call handleclose function, use a semilcolon and call setValue as well
+								onClick={() => {
+									handleClose();
+									setValue(1);
+								}}
+								component={Link}
+								to="/customsoftware"
+								classes={{ root: classes.menuItem }}
+							>
+								Custom Software Development
+							</MenuItem>
+							<MenuItem
+								onClick={() => {
+									handleClose();
+									setValue(1);
+								}}
+								component={Link}
+								to="/mobileapps"
+								classes={{ root: classes.menuItem }}
+							>
+								Mobile App Development
+							</MenuItem>
+							<MenuItem
+								onClick={() => {
+									handleClose();
+									setValue(1);
+								}}
+								component={Link}
+								to="/websites"
+								classes={{ root: classes.menuItem }}
+							>
+								Website Development
+							</MenuItem>
+						</Menu>
 					</Toolbar>
 				</AppBar>
 			</ElevationScroll>
