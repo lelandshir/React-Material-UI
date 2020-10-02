f67cdfff2863e71e42a671400b23d968cf7cc238
lelandshir <lelandshir@gmail.com>
2020-10-02 15:52:21 -0700

    responsive footer complete

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import footerAdornment from "../../assets/Footer Adornment.svg";
 import Grid from "@material-ui/core/Grid";
 import Hidden from "@material-ui/core/Hidden";
+
+import footerAdornment from "../../assets/Footer Adornment.svg";
+import facebook from "../../assets/facebook.svg";
+import twitter from "../../assets/twitter.svg";
+import instagram from "../../assets/instagram.svg";
 
 const useStyles = makeStyles((theme) => ({
 	footer: {
@@ -35,6 +39,22 @@ const useStyles = makeStyles((theme) => ({
 	},
 	gridItem: {
 		margin: "3em",
+	},
+	icon: {
+		height: "4em",
+		width: "4em",
+		[theme.breakpoints.down("xs")]: {
+			height: "2.5em",
+			width: "2.5em",
+		},
+	},
+	socialContainer: {
+		position: "absolute",
+		marginTop: "-6em",
+		right: "1.5em",
+		[theme.breakpoints.down("xs")]: {
+			right: "0.6em",
+		},
 	},
 }));
 export default function Footer(props) {
@@ -204,6 +224,40 @@ export default function Footer(props) {
 				alt="black decorative slash"
 				src={footerAdornment}
 			/>
+			<Grid
+				container
+				justify="flex-end"
+				spacing={2}
+				className={classes.socialContainer}
+			>
+				<Grid
+					item
+					component={"a"}
+					href="https://www.facebook.com"
+					rel="noopener noreferrer"
+					target="_blank"
+				>
+					<img alt="facebook logo" src={facebook} className={classes.icon} />
+				</Grid>
+				<Grid
+					item
+					component={"a"}
+					href="https://www.twitter.com"
+					rel="noopener noreferrer"
+					target="_blank"
+				>
+					<img alt="twitter logo" src={twitter} className={classes.icon} />
+				</Grid>
+				<Grid
+					item
+					component={"a"}
+					href="https://www.instagram.com"
+					rel="noopener noreferrer"
+					target="_blank"
+				>
+					<img alt="instagram logo" src={instagram} className={classes.icon} />
+				</Grid>
+			</Grid>
 		</footer>
 	);
 }
